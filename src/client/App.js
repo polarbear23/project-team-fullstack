d3ebc1d9146d2adf0057f037b80744263239c92e
polarbear23 <xkyd96@gmail.com>
2022-02-23 10:24:51 +0000

    add homepage file, app css file, routing

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,15 @@
+import Home from "./pages/home/Home";
+import Header from "./Header";
+import Footer from "./Footer";
+import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes >
+      <Footer/>
     </div>
   );
 }
