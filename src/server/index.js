2b9a370b1f3220954840d78b07212c848536863f
bravint <bravint@gmail.com>
2022-02-23 12:41:19 +0000

    setup route and controller folder structure

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,17 @@
-require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
+//const axios = require('axios')
 
 const port = process.env.PORT || 4000;
+
+const pokemonRouter = require('./routers/pokemon');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/pokemon', pokemonRouter)
 
 app.get('*', (req, res) => {
     res.json('server running');
