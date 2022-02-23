3228b10e6f42ee7963066ceade4cede3be331015
bravint <bravint@gmail.com>
2022-02-23 11:43:41 +0000

    add prisma schema

---
+++ src/server/controllers/pokemon.js
@@ -0,0 +1,7 @@
+const axios = require('axios');
+
+const initPokemonDatabase = async (req, res) => {
+    
+};
+
+module.exports = { initPokemonDatabase };
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,24 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+//const axios = require('axios')
+
+const port = process.env.PORT || 4000;
+
+const pokemonRouter = require('./routers/pokemon');
 
 const app = express();
 
-app.listen(4000)
+app.use(cors());
+app.use(express.json());
+
+app.use('/pokemon', pokemonRouter)
+
+app.get('*', (req, res) => {
+    res.json('server running');
+});
+
+app.listen(port, () => {
+    console.log(`server started on port ${port}`);
+});
