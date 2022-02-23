f07d4e2e66bfb5ebc7e0c44404acb113f5999f8a
hannah68 <reihaneh.naderi1989@gmail.com>
2022-02-23 11:52:09 +0000

    complete header and footer

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,7 @@
 import Home from "./pages/home/Home";
 import Header from "./Header";
 import Footer from "./Footer";
+import "../client/styles/app.css";
 import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
--- src/client/Header.jsx
+++ src/client/Header.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom"
 
 const Header = () => {
-  return (
-   <header></header>
-  )
-}
+	return (
+		<header>
+			<nav className="navbar-container">
+				
+				<Link to="/" className="navbar__logo">Pokimo & King</Link>
+				
+				<ul className="navbar__lists">
+					<li className="navbar__lists--item">
+						<Link to="/signin">Sign In</Link>
+					</li>
+					<li className="navbar__lists--item">
+						<Link to="/signup">Sign Up</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
 
-export default Header
+export default Header;
