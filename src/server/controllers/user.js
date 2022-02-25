041e9d3c7084f2dc8658d64217e70d13f98e3d73
bravint <bravint@gmail.com>
2022-02-25 11:19:49 +0000

    add authenticateUser controller

--- src/server/controllers/user.js
+++ src/server/controllers/user.js
@@ -1,5 +1,30 @@
-const authenticateUser = async (req, res) => {
+const bcrypt = require('bcrypt');
 
+const checkPassword = async (textPassword, hashedPassword) => {
+    try {
+        return await bcrypt.compare(textPassword, hashedPassword);
+    } catch (error) {
+        console.log(`error in password check`, error);
+        return error;
+    }
+};
+
+const authenticateUser = async (req, res) => {
+    let { username, password } = req.body;
+
+    const foundUser = await prisma.user.findUnique({
+        where: {
+            username,
+        },
+    });
+
+    if (!foundUser) return res.status(401).json('User authentication failed');
+
+    const passwordsMatch = await checkPassword(password, foundUser.password);
+
+    if (!passwordsMatch) return res.status(401).json('User authentication failed');
+
+    res.json(createToken({id: foundUser.id}));
 }
 
 const editUser = async (req, res) => {
