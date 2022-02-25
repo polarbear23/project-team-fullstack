f4d361738bf4cfd21f1b48b468414eb3f551d321
bravint <bravint@gmail.com>
2022-02-25 09:13:08 +0000

    add getComment controller

--- src/server/controllers/post.js
+++ src/server/controllers/post.js
@@ -66,6 +66,22 @@ const deletePost = async (req, res) => {
     });
 };
 
+const getComment = async (req, res) => {
+    const id = req.params.id;
+
+    const selectedComment = await prisma.comment.findUnique({
+        where: {
+            id,
+        },
+    });
+
+    if (!selectedComment) {
+        return res.status(400).json('error');
+    }
+
+    res.status(200).json(selectedComment);
+};
+
 module.exports = {
     getPost,
     createPost,
