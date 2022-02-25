a18f0b67452d1e65714bceb4c3b25a37d00b6246
alexamaingard <maingard.alexa@gmail.com>
2022-02-25 16:34:55 +0100

    add pokemon file in routers and controllers

--- src/server/index.js
+++ src/server/index.js
@@ -9,6 +9,7 @@ const initDatabaseRouter = require('./routers/init');
 const postRouter = require('./routers/post');
 const userRouter = require('./routers/user');
 const ratingRouter = require('./routers/rating');
+const pokemonRouter = require('./routers/pokemon');
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use('/init', initDatabaseRouter);
 app.use('/post', postRouter);
 app.use('/user', userRouter);
 app.use('/rating', ratingRouter);
+app.use('/pokemon', pokemonRouter);
 
 app.get('*', (req, res) => {
     res.json('server running');
