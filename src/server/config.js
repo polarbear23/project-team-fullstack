1eee73cd052fe308bbac5f867890b0a6da312625
alexamaingard <maingard.alexa@gmail.com>
2022-02-25 16:34:35 +0100

    add not found message in config.js

--- src/server/config.js
+++ src/server/config.js
@@ -5,6 +5,7 @@ const SECRET = 'process.env.SECRET';
 const SERVER_ERROR_MESSAGE = {
     UNAUTHORIZED: 'Unauthorized',
     FORBIDDEN: 'Forbidden',
+    NOT_FOUND: 'Not found',
     INTERNAL_SERVER: 'Internal server error',
 };
 
