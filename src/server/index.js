e5ca6ea90748474ea01eb965e51b3c1d51cdefc1
bravint <bravint@gmail.com>
2022-02-28 15:30:17 +0000

    remove old routes from index.js

--- src/server/index.js
+++ src/server/index.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 4000;
 const initDatabaseRouter = require('./routers/init');
 const postRouter = require('./routers/post');
 const userRouter = require('./routers/user');
-// const ratingRouter = require('./routers/rating');
 const pokemonRouter = require('./routers/pokemon');
 
 const app = express();
@@ -21,7 +20,6 @@ app.use(express.json());
 app.use('/init', initDatabaseRouter);
 app.use('/post', postRouter);
 app.use('/user', userRouter);
-// app.use('/rating', ratingRouter);
 app.use('/pokemon', pokemonRouter);
 
 app.get('*', (req, res) => {
