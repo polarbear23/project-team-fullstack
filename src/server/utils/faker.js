19ee98e18fae1716f81ce429e77029cbb8d1ca46
alexamaingard <maingard.alexa@gmail.com>
2022-02-28 16:30:38 +0100

    add fakePost

--- src/server/utils/faker.js
+++ src/server/utils/faker.js
@@ -28,14 +28,48 @@ const fakeProfile = () => {
     return fakedProfile;
 }
 
+const MAX_NUMBER_OF_TAGS_WANTED = 3;
+const MAX_NUMBER_OF_CATEGORIES_WANTED = 2;
 
+const fakePost = () => {
+    const fakedTitle = faker.lorem.sentence();
+    const fakedContent = faker.lorem.sentences();
+    
+    const fakedTags = [];
+    const numberOfTags = generateRandomInt(MAX_NUMBER_OF_TAGS_WANTED);
+
+    for(let i = 0; i < numberOfTags; i++){
+        const fakedTag = faker.lorem.word();
+        fakedTags.push(fakedTag);
+    }
+
+    const postCategories = [];
+    const numberOfCategories = generateRandomInt(MAX_NUMBER_OF_CATEGORIES_WANTED);
+    for(let i = 0; i < numberOfCategories; i++){
+        const randomCategoryIndex = generateRandomInt(CATEGORIES.length - 1);
+        if(postCategories.includes(CATEGORIES[randomCategoryIndex])){
+            i--;
+            continue;
+        }
+        postCategories.push(CATEGORIES[randomCategoryIndex]);
+    }
+
+    const fakedPost = {
+        title: fakedTitle,
+        content: fakedContent,
+        categories: postCategories,
+        tags: fakedTags
+    }
+
+    return fakedPost;
+}
 
 const user = fakeUser();
 console.log(user);
 const profile = fakeProfile();
 console.log(profile);
-// const post = fakePost();
-// console.log(post);
+const post = fakePost();
+console.log(post);
 //const comment = fakeComment();
 //console.log(comment);
 //const commentWithParent = fakeCommentWithParent();
