8c77a535c3b6294e57b0f78912b8fb672b2521ea
alexamaingard <maingard.alexa@gmail.com>
2022-03-01 17:44:38 +0100

    remove DOB div and base handlers in SignUp

--- src/client/pages/forms/Signup.jsx
+++ src/client/pages/forms/Signup.jsx
@@ -1,43 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+const signUpInitialData = {
+	username: "",
+	email: "",
+	password: "",
+}
 
 const Signup = () => {
-  return (
-		<div className='signup'>
-			<form className='signup-form'>
+	const [signUpData, setSignUpData] = useState(signUpInitialData);
+
+
+
+	const handleChange = event => {
+		const { name, value } = event.target;
+
+		setSignUpData({...signUpData, [name]: value});
+	}
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+
+		setSignUpData(signUpData);
+	}
+
+  	return (
+		<div className="signup">
+			<form className="signup-form">
                 <h1>Register</h1>
 				<div className="input-groups">
 					<label htmlFor="username">Username:</label>
-					<input type="text" id='username' className='input'/>
+					<input 
+						type="text" 
+						id="username" 
+						className="input"
+						name="username"
+						onChange={handleChange}
+						required
+					/>
 				</div>
 				<div className="input-groups">
 					<label htmlFor="email">Email:</label>
-					<input type="text" id='email' className='input'/>
+					<input 
+						type="text" 
+						id="email" 
+						className="input"
+						name="email"
+						onChange={handleChange}
+						required
+					/>
 				</div>
 				<div className="input-groups">
 					<label htmlFor="password">Password:</label>
-					<input type="text" id='password' className='input'/>
-				</div>
-				<div className="input-groups">
-					<label htmlFor="dob_month" className='dob_month'>Date of birth:</label>
-					<select name="dob_month" className="dob" aria-label="Month">
-						<option value="0" selected="selected">
-							&nbsp;
-						</option>
-						<option value="1">January</option>
-						<option value="2">February</option>
-						<option value="3">March</option>
-						<option value="4">April</option>
-						<option value="5">May</option>
-						<option value="6">June</option>
-						<option value="7">July</option>
-						<option value="8">August</option>
-						<option value="9">September</option>
-						<option value="10">October</option>
-						<option value="11">November</option>
-						<option value="12">December</option>
-					</select>
-                    <input type="text" className="dob" name="dob_day" pattern="\d*" size="4" maxlength="2" placeholder="Day"></input>
-                    <input type="text" className="dob" name="dob_year" pattern="\d*" size="6" maxlength="4" placeholder="Year"></input>
+					<input 
+						type="text" 
+						id="password" 
+						className="input"
+						name="password"
+						onChange={handleChange}
+						required
+					/>
 				</div>
 				<input type="checkbox" />
 				<span className='terms'>I agree to the terms and privacy policy.</span>
