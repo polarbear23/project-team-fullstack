790331c06032b0fa45dd0eddc4a7401ab32a4b20
bravint <bravint@gmail.com>
2022-03-01 17:32:43 +0000

    profile component added add token, user states to app.jsx

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/home/Home';
@@ -5,6 +6,7 @@ import Header from './Header';
 import Footer from './Footer';
 import LeftMenu from './LeftMenu';
 import Leaderboard from './pages/leaderboard/Leaderboard';
+import { CreateProfile } from './pages/register/CreateProfile';
 import Signup from './pages/forms/Signup';
 import SignIn from './pages/forms/SignIn';
 import Forum from './pages/forum/Forum';
@@ -12,16 +14,41 @@ import Forum from './pages/forum/Forum';
 import '../client/styles/app.css';
 
 export const App = () => {
+    const [token, setToken] = useState('');
+    const [user, setUser] = useState({});
+
     return (
         <div className="app">
-            <Header />
+            <Header user={user} />
             <LeftMenu />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/signin" element={<SignIn />} />
-                <Route path="/forum" element={<Forum />} />
-                <Route path="/leaderboard" element={<Leaderboard />} />
+                <Route
+                    path="/create-profile"
+                    element={
+                        <CreateProfile
+                            token={token}
+                            user={user}
+                            setUser={setUser}
+                        />
+                    }
+                />
+                <Route
+                    path="/signup"
+                    element={<Signup setToken={setToken} setUser={setUser} />}
+                />
+                <Route
+                    path="/signin"
+                    element={<SignIn setToken={setToken} setUser={setUser} />}
+                />
+                <Route
+                    path="/forum"
+                    element={<Forum token={token} user={user} />}
+                />
+                <Route
+                    path="/leaderboard"
+                    element={<Leaderboard token={token} user={user} />}
+                />
             </Routes>
             <Footer />
         </div>
--- src/client/pages/forms/SignIn.jsx
+++ src/client/pages/forms/SignIn.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-
 const SignIn = () => {
-  return (
-		<div className="signIn">
+    return (
+        <div className="signIn">
             <div className="signin-container">
-                <form className='signin-form'>
+                <form className="signin-form">
                     <h1>Sign In</h1>
                     <div className="input-groups">
                         <label htmlFor="username">Username:</label>
@@ -14,15 +12,19 @@ const SignIn = () => {
                         <label htmlFor="password">Password:</label>
                         <input type="text" id="password" className="input" />
                     </div>
-                    <button type='submit' className='signin-btn'>Sign In</button>
+                    <button type="submit" className="signin-btn">
+                        Sign In
+                    </button>
                 </form>
                 <div className="new-register">
-                    <p className='signin-text'>No account? No worries.</p>
-                    <button type='submit' className='new-register-btn'>New Account</button>
+                    <p className="signin-text">No account? No worries.</p>
+                    <button type="submit" className="new-register-btn">
+                        New Account
+                    </button>
                 </div>
             </div>
-		</div>
-	);
-}
+        </div>
+    );
+};
 
-export default SignIn
+export default SignIn;
