5f9442c92eac658df531db9a0afcc54cf65fa680
alexamaingard <maingard.alexa@gmail.com>
2022-03-02 18:47:35 +0100

    modify client config to object exporting

--- src/client/config.js
+++ src/client/config.js
@@ -1,30 +1,37 @@
-export class USER_URL {
-    static USER_ROOT = 'http://localhost:4000/user/'
-    static REGISTER = 'http://localhost:4000/user/register'
-    static LOGIN = 'http://localhost:4000/user/login'
-    static PROFILE = 'http://localhost:4000/user/profile'
+const USER_URL = {
+    USER_ROOT: 'http://localhost:4000/user/',
+    REGISTER: 'http://localhost:4000/user/register',
+    LOGIN: 'http://localhost:4000/user/login',
+    PROFILE: 'http://localhost:4000/user/profile'
 }
 
-export class INT_LINK {
-    static HOME = '/'
-    static CREATE_PROFILE = '/register/profile'
-    static CREATE_USER = '/register/user'
-    static LOGIN = '/login'
-    static FORUM = '/forum'
-    static LEADERBOARD = '/leaderboard'
-    static NOT_FOUND = '*'
-    static PROFILE = '/profile'
+const INT_LINK =  {
+    HOME: '/',
+    CREATE_PROFILE: '/register/profile',
+    CREATE_USER: '/register/user',
+    LOGIN: '/login',
+    FORUM: '/forum',
+    LEADERBOARD: '/leaderboard',
+    NOT_FOUND: '*',
+    PROFILE: '/profile'
 }
 
-export class LOCAL_STORAGE {
-    static TOKEN = 'token'
-    static USER_ID = 'userId'
+const LOCAL_STORAGE = {
+    TOKEN: 'token',
+    USER_ID: 'userId'
 }
 
-export class FETCH_METHOD {
-    static GET = 'GET'
-    static POST = 'POST'
-    static PATCH = 'PATCH'
-    static PUT = 'PUT'
-    static DELETE = 'DELETE'
+const FETCH_METHOD = {
+    GET: 'GET',
+    POST: 'POST',
+    PATCH: 'PATCH',
+    PUT: 'PUT',
+    DELETE: 'DELETE'
+}
+
+module.exports = {
+    USER_URL,
+    INT_LINK,
+    LOCAL_STORAGE,
+    FETCH_METHOD
 }
