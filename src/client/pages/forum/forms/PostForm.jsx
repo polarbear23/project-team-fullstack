9aa7f0301007e7bb7670005694f6cb07bf8358ff
polarbear23 <xkyd96@gmail.com>
2022-03-03 17:39:06 +0000

    modify post function

--- src/client/pages/forum/forms/PostForm.jsx
+++ src/client/pages/forum/forms/PostForm.jsx
@@ -1,109 +1,113 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Category from '../Category';
-import Tag from '../Tag';
+import Category from "../Category";
+import Tag from "../Tag";
 
-import { HTTP_METHOD, FORUM_URL, LOCAL_STORAGE } from '../../../config';
+import { HTTP_METHOD, FORUM_URL, LOCAL_STORAGE } from "../../../config";
 
 const PostForm = (props) => {
-    const { posts, setPosts, setShowForm } = props;
+  const { posts, setPosts, setShowForm } = props;
 
-    const initialPost = {
-        title: '',
-        content: '',
+  const initialPost = {
+    title: "",
+    content: "",
+  };
+
+  const [tags, setTags] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [newPost, setNewPost] = useState(initialPost);
+
+  const clearForm = () => {
+    setNewPost(initialPost);
+    setCategories([]);
+    setTags([]);
+  };
+
+  const changeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setNewPost({ ...newPost, [name]: value });
+
+    if (
+      name === "Gaming" ||
+      name === "Fan-Fiction" ||
+      name === "Cosplay" ||
+      name === "Manga" ||
+      name === "Tv/Film"
+    ) {
+      setCategories([...categories, name]);
+    }
+  };
+
+  const submitNewPostHandler = (event) => {
+    event.preventDefault();
+
+    const postToCreate = {
+      title: newPost.title,
+      content: newPost.content,
+      categories: categories,
+      tags: tags,
+      userId: Number(localStorage.getItem(LOCAL_STORAGE.USER_ID)),
     };
 
-    const [tags, setTags] = useState([]);
-    const [categories, setCategories] = useState([]);
-    const [newPost, setNewPost] = useState(initialPost);
+    fetchNewPost(postToCreate);
 
-	const clearForm = () => {
-		setNewPost(initialPost);
-		setCategories([]);
-		setTags([]);
-	}
+    clearForm();
 
-    const changeHandler = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
+    setShowForm(false);
+  };
 
-        setNewPost({ ...newPost, [name]: value });
+  const fetchNewPost = async (postToCreate) => {
+    const response = await fetch(FORUM_URL.POST, {
+      method: `${HTTP_METHOD.POST}`,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem(LOCAL_STORAGE.TOKEN),
+      },
+      body: JSON.stringify(postToCreate),
+    });
 
-        if (
-            name === 'Gaming' ||
-            name === 'Fan-Fiction' ||
-            name === 'Cosplay' ||
-            name === 'Manga' ||
-            name === 'Tv/Film'
-        ) {
-            setCategories([...categories, name]);
-        }
-    };
+    const data = await response.json();
 
-    const submitNewPostHandler = (event) => {
-        event.preventDefault();
+    setPosts([...posts, data.data]);
+  };
 
-        const newPost = {
-            title: newPost.title,
-            content: newPost.content,
-            categories: categories,
-            tags: tags,
-            userId: Number(localStorage.getItem(LOCAL_STORAGE.USER_ID)),
-        };
+  return (
+    <form className="create-post-form" onSubmit={submitNewPostHandler}>
+      <input
+        type="text"
+        placeholder="Title"
+        name="title"
+        value={newPost.title}
+        onChange={changeHandler}
+      />
+      <textarea
+        placeholder="Text"
+        name="content"
+        className="post-form-textarea"
+        value={newPost.content}
+        onChange={changeHandler}
+      ></textarea>
 
-        fetchNewPost(newPost);
+      <Category changeHandler={changeHandler} />
 
-		clearForm();
+      <Tag tags={tags} setTags={setTags} />
 
-		setShowForm(false);
-    };
-
-    const fetchNewPost = async (newPost) => {
-        const response = await fetch(FORUM_URL.POST, {
-            method: `${HTTP_METHOD.POST}`,
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: localStorage.getItem(LOCAL_STORAGE.TOKEN),
-            },
-            body: JSON.stringify(newPost),
-        });
-
-        const data = await response.json();
-
-        setPosts([...posts, data.data]);
-    };
-
-    return (
-        <form className="create-post-form" onSubmit={submitNewPostHandler}>
-            <input
-                type="text"
-                placeholder="Title"
-                name="title"
-                value={newPost.title}
-                onChange={changeHandler}
-            />
-            <textarea
-                placeholder="Text"
-                name="content"
-                className="post-form-textarea"
-                value={newPost.content}
-                onChange={changeHandler}
-            ></textarea>
-
-            <Category changeHandler={changeHandler} />
-
-            <Tag tags={tags} setTags={setTags} />
-
-			<div className="group-post-btn">
-				<button type="button" className="save-post-btn" onClick={() => setShowForm(false)}>
-					Cancel
-				</button>
-				<button type="submit" className="submit-post-btn">
-					Post
-				</button>
-			</div>
-        </form>
-    );
+      <div className="group-post-btn">
+        <button
+          type="button"
+          className="save-post-btn"
+          onClick={() => setShowForm(false)}
+        >
+          Cancel
+        </button>
+        <button type="submit" className="submit-post-btn">
+          Post
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default PostForm;
